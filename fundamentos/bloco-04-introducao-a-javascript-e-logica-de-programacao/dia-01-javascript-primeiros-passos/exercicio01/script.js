7598f44d051deddcd3a9b3e21f24388b489285fd
karl-no <karl.publicidade@gmail.com>
2022-05-12 16:12:52 -0300

    Bloco 4 de Fundamentos criado

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio01/script.js
@@ -0,0 +1,14 @@
+const myName = "Karl";
+
+const birthCity = "Barreiras";
+
+let birthYear = 1989;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Turku";
