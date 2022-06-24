140200518005464a4a0aa4f268a9a3546f7b0feb
karl-no <karl.publicidade@gmail.com>
2022-06-23 22:56:54 -0300

    Exemplos de sort ()

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exemplos/exemplo15.js
@@ -0,0 +1,6 @@
+const points = [40, 100, 1, 5, 25, 10];
+points.sort((a, b) => a - b);
+console.log(points); // [1, 5, 10, 25, 40, 100]
+
+points.sort((a, b) => b - a);
+console.log(points); // [ 100, 40, 25, 10, 5, 1 ]
