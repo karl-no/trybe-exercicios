f9f91ecdc17308681ffac54b1b68de262b8f771a
karl-no <karl.publicidade@gmail.com>
2022-07-18 22:45:51 -0300

    Exercicios do bloco 10, dia 1, de front-end

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercicios-react-02/src/Content.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+const conteudos = [
+    {
+      conteudo: 'High Order Functions',
+      bloco: 8,
+      status: 'Aprendido'
+    },
+    {
+      conteudo: 'Composicao de Componentes',
+      bloco: 11,
+      status: 'Aprendendo',
+    },
+    {
+      conteudo: 'Composicao de Estados',
+      bloco: 12,
+      status: 'Aprenderei'
+    },
+    {
+      conteudo: 'Redux',
+      bloco: 16,
+      status: 'Aprenderei'
+    },
+  ];
+
+class Content extends Component {
+    render() {
+        return (
+            <div>
+                { conteudos.map((callback) => (
+                    <div key={callback.conteudo} className="card">
+                        <h4>{`O conteúdo é: ${callback.conteudo}`}</h4>
+                        <p>{`Status: ${callback.status}`}</p>
+                        <p>{`Bloco: ${callback.bloco}`}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Content;
