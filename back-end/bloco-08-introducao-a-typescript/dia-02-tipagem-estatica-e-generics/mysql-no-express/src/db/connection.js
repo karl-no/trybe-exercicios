e3775cf494f38848f3149ad93e22cef95fa365e1
karl-no <karl.publicidade@gmail.com>
2023-01-11 21:43:16 -0300

    Exemplos do bloco de Programação Orientada a Objetos - dia 1

---
+++ back-end/bloco-08-introducao-a-typescript/dia-02-tipagem-estatica-e-generics/mysql-no-express/src/db/connection.js
@@ -0,0 +1,16 @@
+// src/db/connection.js
+
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: MYSQL_HOST,
+  port: MYSQL_PORT,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = connection;
